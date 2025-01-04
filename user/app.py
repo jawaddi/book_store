15fafdb66c033b92927d404a844e56f161c9bf1f
j.lamin@sanadtech.com <jawadlamin83@gmail.com>
2025-01-04 13:08:26 +0100

    create a route for getting all the user

--- user/app.py
+++ user/app.py
@@ -1,13 +1,27 @@
 from flask import Flask
 from routes import user_Blueprint
+from flask_migrate import Migrate
+from dotenv import load_dotenv
 import os
 import models
 
+load_dotenv()
+
+file_path = os.path.abspath(os.getcwd())+r"\database\user.db"
+print(file_path)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+app.config['SQLALCHEMY_DATABASE_URI'] =f'sqlite:///{file_path}'
+#os.getenv('SQLALCHEMY_DATABASE_URI')
+
 
 models.init_app(app)
 app.register_blueprint(user_Blueprint)
+
+migrate = Migrate(app,models.db) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
--- user/routes.py
+++ user/routes.py
@@ -1,7 +1,14 @@
-from flask import Blueprint
-
+from flask import Blueprint,jsonify
+from models import db,User
 user_Blueprint = Blueprint("user_api_routes",__name__,url_prefix="/api/user")
 
-@user_Blueprint.route("/")
-def index():
-    return "hello"
+@user_Blueprint.route("/all",methods=['GET'])
+def get_all_users():
+    all_user = User.query.all()
+    users = [user.serialize() for user in all_user]
+    response = {
+        "message":"get all the users",
+        "users":users
+    }
+
+    return jsonify(response)
