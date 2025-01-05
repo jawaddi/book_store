d5282d7ab68afd28a79a606327fc2de0fecda479
j.lamin@sanadtech.com <jawadlamin83@gmail.com>
2025-01-05 13:36:13 +0100

    Add Book database model Adress #2

--- book/app.py
+++ book/app.py
@@ -3,16 +3,20 @@ from routes import book_blueprint
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
+from models import db,Book,init_app
 
 load_dotenv()
-
+file_path = os.path.abspath(os.getcwd())+r"\database\book.db"
 app = Flask(__name__)
 
 app.config['SECRECT_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/book.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{file_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.register_blueprint(book_blueprint)
 
+init_app(app)
+
+migrate = Migrate(app,db)
 if __name__ == '__main__':
     app.run(debug=True,port=5003)
---
+++ book/models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def init_app(app):
+    db.app = app
+    db.init_app(app)
+
+class Book(db.Model):
+    id=db.Column(db.Integer,primary_key=True,nullable=False)
+    name=db.Column(db.String(255),unique=True,nullable=False)
+    slug=db.Column(db.String(255),unique=True,nullable=False)
+    prince=db.Column(db.Integer,nullable=True)
+    image=db.Column(db.String(255))
+
+    def __repr__(self):
+        f'<book {self.id}> {self.name} >'
+
+    def serialize(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+            'slug':self.slug,
+            'prince':self.prince,
+            'image':self.imag
+        }
