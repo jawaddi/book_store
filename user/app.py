f42b23a571505f5c3b8eec52b217bc1262126da2
j.lamin@sanadtech.com <jawadlamin83@gmail.com>
2025-01-08 15:50:48 +0100

    user can log in and log out Fixed#1

--- user/app.py
+++ user/app.py
@@ -1,56 +1,50 @@
 from flask import Flask
-from flask_login import LoginManager 
-from routes import user_Blueprint
+from flask.sessions import SecureCookieSessionInterface
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from dotenv import load_dotenv
-import os
 import models
-import base64
-
-
-
+from routes import user_Blueprint
+import os
 load_dotenv()
 
 file_path = os.path.abspath(os.getcwd())+r"\database\user.db"
-print(file_path)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-app.config['SQLALCHEMY_DATABASE_URI'] =f'sqlite:///{file_path}'
-#os.getenv('SQLALCHEMY_DATABASE_URI')
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{file_path}'
 models.init_app(app)
 app.register_blueprint(user_Blueprint)
 login_manager = LoginManager(app)
-migrate = Migrate(app,models.db) 
+migrate = Migrate(app, models.db)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.query.filter_by(id=user_id).first()
+
 
 @login_manager.request_loader
 def load_user_from_request(request):
-
-    # first, try to login using the api_key url arg
-    api_key = request.args.get('api_key')
-    if api_key:
-        user = models.User.query.filter_by(api_key=api_key).first()
-        if user:
-            return user
-
-    # next, try to login using Basic Auth
     api_key = request.headers.get('Authorization')
     if api_key:
         api_key = api_key.replace('Basic ', '', 1)
-        try:
-            api_key = base64.b64decode(api_key)
-        except TypeError:
-            pass
         user = models.User.query.filter_by(api_key=api_key).first()
         if user:
             return user
 
-    # finally, return None if both methods did not login the user
     return None
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
+class CustomSessionInterface(SecureCookieSessionInterface):
+    """Prevent creating session from API requests."""
+
+    def save_session(self, *args, **kwargs):
+        if g.get('login_via_header'):
+            return
+        return super(CustomSessionInterface, self).save_session(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    app.run(Debug=True, port=5001)
--- user/routes.py
+++ user/routes.py
@@ -4,111 +4,82 @@ from models import db,User
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import login_user, current_user, logout_user
 
-user_Blueprint = Blueprint("user_api_routes",__name__,url_prefix="/api/user")
+user_Blueprint = Blueprint('user_api_routes', __name__,
+                           url_prefix='/api/user')
 
-# get all the users
-@user_Blueprint.route("/all",methods=['GET'])
+
+@user_Blueprint.route('/all', methods=['GET'])
 def get_all_users():
     all_user = User.query.all()
     users = [user.serialize() for user in all_user]
     response = {
-        "message":"get all the users",
-        "users":users
+        'message': 'Returning all users',
+        'result': users
     }
-
     return jsonify(response)
 
 
-#create new user
-
-@user_Blueprint.route("/create",methods=['POST'])
-
+@user_Blueprint.route('/create', methods=['POST'])
 def create_user():
     try:
-        print('jawad',request.form)
         user = User()
         user.username = request.form["username"]
-        user.password = generate_password_hash(request.form["password"],method='pbkdf2:sha256')
-        user.is_admin = True
+        user.password = generate_password_hash(request.form['password'],
+                                               method='pbkdf2:sha256')
+
+        user.is_admin = False
 
         db.session.add(user)
         db.session.commit()
 
-        response = {
-            "message":"User has been created successfully",
-            "user":user.serialize()
-        }
+        response = {'message': 'User Created', 'result': user.serialize()}
     except Exception as e:
         print(str(e))
-        response = {
-
-            "message":"something went wrong while creating user "
-        }
-        return make_response(jsonify(response), 400)  # 400 Bad Request
-
+        response = {'message': 'Error in creating response'}
     return jsonify(response)
 
-#create  login endpoint
 
-@user_Blueprint.route("/login",methods=['POST'])
-
+@user_Blueprint.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
 
     user = User.query.filter_by(username=username).first()
-
     if not user:
-        response =  {
-                "message":f'{username} is not exists! :( )'
-            }
-        return make_response(jsonify(response),401)
-    if check_password_hash(user.password,password) :
+        response = {'message': 'username does not exists'}
+        return make_response(jsonify(response), 401)
+    if check_password_hash(user.password, password):
         user.update_api_key()
         db.session.commit()
-
         login_user(user)
+        response = {'message': 'logged in ', 'api_key': user.api_key}
+        return make_response(jsonify(response), 200)
 
-        response = {
-            "message":"logged in",
-            "api_key":user.api_key
-        }
-        return make_response(jsonify(response),200)
-    
-    
-    response = {"message":"access denied"}
-
-    return make_response(jsonify(response),401)
+    response = {'message': 'Access denied'}
+    return make_response(jsonify(response), 401)
 
 
-# log out
-
-@user_Blueprint.route("/logout",methods=['POST'])
-
+@user_Blueprint.route('/logout', methods=['POST'])
 def logout():
     if current_user.is_authenticated:
         logout_user()
-        response = {"message":"logged out"}
-        return make_response(jsonify(response),200)
+        return jsonify({'message': 'logged out'})
+    return jsonify({'message': 'No user logged in'}), 401
 
-    return make_response(jsonify({"message":"No user logged out"}),401)
 
-@user_Blueprint.route("/<username>/exists",methods=['GET'])
+@user_Blueprint.route('/<username>/exists', methods=['GET'])
 def user_exists(username):
     user = User.query.filter_by(username=username).first()
-    response = {"message":"user not found"}
-    if not user:
-        return make_response(jsonify(response),404)
-    
-    response = {"message":"user exists","user":user}
-    return make_response(jsonify(response),200)
+    if user:
+        return jsonify({"result": True}), 200
 
-@user_Blueprint.route("/",methods=["GET"])
+    return jsonify({"result": False}), 404
+
+
+@user_Blueprint.route('/', methods=['GET'])
 def get_current_user():
-    # print(current_user)
-    # print(current_user.is_authenticated)
-    # print(current_user.serialize())
+    
     if current_user.is_authenticated:
-        return make_response(jsonify({"user":current_user.serialize()}),200)
+        return jsonify({'user': current_user.serialize()}), 200
     else:
-        return make_response(jsonify({"message":"no user logged in "}),200)
+        return jsonify({'message': "User not logged in"}), 401
